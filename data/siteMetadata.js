3f3d866e2aecec6f4d183afe2aeb51968a7cd6c3
Rossi Meacham <rossimeacham@gmail.com>
2022-08-23 15:44:44 -0400

    update naming

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,11 +1,11 @@
 const siteMetadata = {
-  title: 'Metastash Blog',
+  title: 'ABCs of NFTs',
   author: 'metastash',
-  headerTitle: 'metastash',
-  description: 'Musings',
+  headerTitle: 'ABCs of NFT',
+  description: 'Musings on the state of NFTs and Crypto',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteUrl: 'https://abcsofnft.com',
   siteRepo: 'https://github.com/corradorossi/metastashblog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.jpg',
